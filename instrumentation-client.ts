96a70ab820475d9535103db9152a90bdd7bc9e3a
Minhkh0inee <mkhoi1776@gmail.com>
2026-05-10 21:04:05 +0700

    Update Instrumentation

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -21,7 +21,7 @@ export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
 
 import posthog from "posthog-js";
 
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_TOKEN!, {
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN!, {
   api_host: "/ingest",
   ui_host: "https://us.posthog.com",
   defaults: "2026-01-30",
