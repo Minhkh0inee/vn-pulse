64506f64a70c50247590616f46df1a6b561c103d
Minhkh0inee <mkhoi1776@gmail.com>
2026-05-10 17:36:32 +0700

    Add Log

--- app/postHogProvider.tsx
+++ app/postHogProvider.tsx
@@ -1,14 +1,15 @@
 'use client'
 
-import { usePathname, useSearchParams } from "next/navigation"
 import { useEffect } from "react"
 
 import posthog from 'posthog-js'
 import { PostHogProvider as PHProvider } from '@posthog/react'
 
 export function PostHogProvider({ children }: { children: React.ReactNode }) {
+  console.log("post hog token outside", process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN)
   useEffect(() => {
-    const token = process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN
+    const token = process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN;
+    console.log("post hog token", token)
     if (!token) return
     posthog.init(token, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
